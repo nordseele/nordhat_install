4c0eb98e729dcb32c1ae84b7039fe6c1fb0ed117
Matthias Budde <matthiasbudde@macbook-pro-de-matthias.home>
2020-02-04 15:21:36 +0100

    Moved linux files to this repo

--- install/norns/scripts/hat_prepare.sh
+++ install/norns/scripts/hat_prepare.sh
@@ -4,9 +4,9 @@ git clone https://github.com/nordseele/norns-linux-bits.git
 git clone --depth 1 --branch rpi-4.14.y-rt https://github.com/raspberrypi/linux
 cd linux
 git checkout 22bb67b8e2e809d0bb6d435c1d20b409861794d2
-cp ~/norns-linux-bits/drivers-staging-fbtft/* /home/we/linux/drivers/staging/fbtft/
-cp ~/norns-linux-bits/arch-arm-configs/bcm2709_defconfig /home/we/linux/arch/arm/configs/bcm2709_defconfig
-cp ~/norns-linux-bits/.config /home/we/linux/.config
+cp /home/we/nordhat_install/install/norns/files/drivers-staging-fbtft/* /home/we/linux/drivers/staging/fbtft/
+cp /home/we/nordhat_install/install/norns/files/arch-arm-configs/bcm2709_defconfig /home/we/linux/arch/arm/configs/bcm2709_defconfig
+cp /home/we/nordhat_install/install/norns/files/.config /home/we/linux/.config
 
 cd ~/linux
 export KERNEL=kernel7
